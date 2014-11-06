f746578de61c6c2ae6f6939932f8d747817cc237
rstitan <rajashekar333@gmail.com>
2014-11-05 19:54:41 -0500

    added files

---
+++ comcast/SSD/openstack/scripts/testfile.sh
@@ -0,0 +1,26 @@
+8 rguntu002c  1258410764   272 Nov  5 15:12 SSD
+drwxr-xr-x    6 rguntu002c  1258410764   204 Nov  3 15:02 prod
+drwxr-xr-x    3 rguntu002c  1258410764   102 Nov  4 10:15 yumrepo
+HQSML-146487:github rguntu002c$ cd SSD/
+HQSML-146487:SSD rguntu002c$ ls
+README.md	old-scripts	openstack	scripts		security_groups
+HQSML-146487:SSD rguntu002c$ ls -al
+total 8
+drwxr-xr-x   8 rguntu002c  1258410764  272 Nov  5 15:12 .
+drwxr-xr-x   5 rguntu002c  1258410764  170 Nov  5 15:12 ..
+drwxr-xr-x  13 rguntu002c  1258410764  442 Nov  5 16:44 .git
+-rw-r--r--   1 rguntu002c  1258410764   20 Nov  5 15:12 README.md
+drwxr-xr-x   3 rguntu002c  1258410764  102 Nov  5 15:12 old-scripts
+drwxr-xr-x   4 rguntu002c  1258410764  136 Nov  5 15:12 openstack
+drwxr-xr-x   9 rguntu002c  1258410764  306 Nov  5 16:44 scripts
+drwxr-xr-x  16 rguntu002c  1258410764  544 Nov  5 15:12 security_groups
+HQSML-146487:SSD rguntu002c$ ls
+README.md	old-scripts	openstack	scripts		security_groups
+HQSML-146487:SSD rguntu002c$ for file in `ls|grep -v READ`; do
+> cp -r file /Users/rguntu002c/Documents/github_personal/Main/comcast/SSD;
+> done
+cp: file: No such file or directory
+cp: file: No such file or directory
+cp: file: No such file or directory
+cp: file: No such file or directory
+HQSML-146487:SSD rguntu002c$ for file in `ls|grep -v READ`; do cp -r $file /Users/rguntu002c/Documents/github_personal/Main/comcast/S
